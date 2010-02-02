c88b05bee20024b4393ca9ccf86a20e96f854d2e
Henrik Thostrup Jensen <thostrup@gmail.com>
2010-02-02 20:56:42 +0100

    add date module with initial card deck

---
+++ machinemadness/data/decks.py
@@ -0,0 +1,76 @@
+
+from machinemadness.game import card
+
+# card decks
+
+# 10 of each cards
+deck1 = frozenset(
+    card.Forward3(89),
+    card.Forward3(88),
+    card.Forward3(87),
+    card.Forward3(86),
+    card.Forward3(85),
+    card.Forward3(84),
+    card.Forward3(83),
+    card.Forward3(82),
+    card.Forward3(81),
+    card.Forward3(80),
+
+    card.Forward2(79),
+    card.Forward2(78),
+    card.Forward2(77),
+    card.Forward2(76),
+    card.Forward2(75),
+    card.Forward2(74),
+    card.Forward2(73),
+    card.Forward2(72),
+    card.Forward2(71),
+    card.Forward2(70),
+
+    card.Forward1(69),
+    card.Forward1(68),
+    card.Forward1(67),
+    card.Forward1(65),
+    card.Forward1(64),
+    card.Forward1(63),
+    card.Forward1(62),
+    card.Forward1(61),
+    card.Forward1(60),
+
+    card.Backwards1(40),
+    card.Backwards1(40),
+    card.Backwards1(40),
+    card.Backwards1(40),
+    card.Backwards1(40),
+    card.Backwards1(40),
+    card.Backwards1(40),
+    card.Backwards1(40),
+    card.Backwards1(40),
+    card.Backwards1(40),
+
+    card.RotateRight(40),
+    card.RotateRight(40),
+    card.RotateRight(40),
+    card.RotateRight(40),
+    card.RotateRight(40),
+    card.RotateRight(40),
+    card.RotateRight(40),
+    card.RotateRight(40),
+    card.RotateRight(40),
+    card.RotateRight(40),
+
+    card.RotateLeft(40),
+    card.RotateLeft(40),
+    card.RotateLeft(40),
+    card.RotateLeft(40),
+    card.RotateLeft(40),
+    card.RotateLeft(40),
+    card.RotateLeft(40),
+    card.RotateLeft(40),
+    card.RotateLeft(40),
+    card.RotateLeft(40)
+
+)
+
+
+
